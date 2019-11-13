ee9500bada6c49102339cc98c83b2b59fb71e3d3
Robin Jacobs <robin@robinj.be>
2019-11-13 19:12:18 +0000

    Split up App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import useInterval from 'use-interval';
 import './App.css';
-import List from './List';
 import uuid from 'uuid/v4';
 import { VOTE_MODES, SUBMENUS, PATH_MAX_LENGTH } from './constants';
 import { exportToConfluenceWiki, exportToJson, exportToMarkdown } from './export';
 import Cache from "./Cache";
-import {httpPost, httpPut, copyToClipboard, httpPatch, httpDelete} from './utils';
+import {copyToClipboard, httpPut} from './utils';
+import Retrospective from './Retrospective';
 
 const trimSlashes = str => str.replace(/^\//, '').replace(/\/$/, '');
 const getRetroIdFromUrl = () => {
@@ -51,73 +50,14 @@ const alertAndCopy = text => copyToClipboard(text)
   .then(() => alert(text))
   .catch(() => alert(text));
 
-const updateItemText = (type, id, text) => httpPatch(`${window.API_BASE}/${type}/${id}`, { text })
-  .catch(alert);
-
-const deleteItem = (type, id) => httpDelete(`${window.API_BASE}/${type}/${id}`)
-  .catch(alert);
-
 function App() {
+  const [good, setGood] = useState(initialData.good);
+  const [bad, setBad] = useState(initialData.bad);
+  const [actions, setActions] = useState(initialData.actions);
   const [ title, setTitle ] = useState(initialData.title);
-  const [ good, setGood ] = useState(initialData.good);
-  const [ bad, setBad ] = useState(initialData.bad);
-  const [ actions, setActions ] = useState(initialData.actions);
   const [ voteMode, setVoteMode ] = useState(initialData.voteMode);
   const [ openedSubmenu, setOpenedSubmenu ] = useState(null);
   const [ error, setError ] = useState(null);
-
-  const share = () => {
-    if (navigator.share) {
-      const nItems = good.length + bad.length + actions.length;
-
-      return navigator.share({
-        title: title || 'Sprint Retrospective',
-        text: `Collaborate on this sprint retrospective! It currently contains ${nItems} items.`,
-        url: window.location
-      })
-      .catch(() => shareFallback());
-    }
-
-    return shareFallback();
-  };
-
-  const addGood = text => {
-    httpPost(`${window.API_BASE}/good`, { text })
-      .catch(alert);
-
-    setGood([...good, { text }]);
-  };
-  const addBad = text => {
-    httpPost(`${window.API_BASE}/bad`, { text })
-      .catch(alert);
-
-    setBad([...bad, { text }]);
-  };
-  const addAction = text => {
-    httpPost(`${window.API_BASE}/action`, { text })
-      .catch(alert);
-
-    setActions([...actions, { text }]);
-  };
-
-  const upvoteItem = (id, type, setter, list) => {
-    httpPost(`${window.API_BASE}/${type}/${id}/up`)
-      .catch(alert);
-
-    setter(list.map(item => ({
-      ...item,
-      up: item.up + (item.id === id ? 1 : 0)
-    })));
-  };
-  const downvoteItem = (id, type, setter, list) => {
-    httpPost(`${window.API_BASE}/${type}/${id}/down`)
-      .catch(alert);
-
-    setter(list.map(item => ({
-      ...item,
-      down: item.down + (item.id === id ? 1 : 0)
-    })));
-  };
 
   const updateTitle = title => {
     httpPut(`${window.API_BASE}/title`, { title })
@@ -142,23 +82,20 @@ function App() {
     setOpenedSubmenu(submenu);
   };
 
-  useInterval(() => {
-    fetch(window.API_BASE)
-      .then(res => res.json())
-      .then(({id, good, bad, actions, title, voteMode }) => {
-        setGood(good);
-        setBad(bad);
-        setActions(actions);
-        setTitle(title);
-        setVoteMode(voteMode);
-        setError(null);
+  const share = ({ title, good, bad, actions }) => {
+    if (navigator.share) {
+      const nItems = good.length + bad.length + actions.length;
 
-        if (id) {
-          cache.setIfModified(id, { id, good, bad, actions, title, voteMode })
-              .catch(console.error);
-        }
-      }).catch(setError);
-  }, 1000);
+      return navigator.share({
+        title: title || 'Sprint Retrospective',
+        text: `Collaborate on this sprint retrospective! It currently contains ${nItems} items.`,
+        url: window.location
+      })
+        .catch(() => shareFallback());
+    }
+
+    return shareFallback();
+  };
 
   return (
     <main className={!!error && 'error'}>
@@ -197,44 +134,19 @@ function App() {
       {!!error &&
         <p id="error">{error.message}</p>
       }
-      <article>
-        <section id="good">
-          <h2>Good 👍</h2>
-          <List
-            items={good}
-            addItem={addGood}
-            voteMode={voteMode}
-            upvoteItem={id => upvoteItem(id, 'good', setGood, good)}
-            downvoteItem={id => downvoteItem(id, 'good', setGood, good)}
-            updateItemText={(id, text) => updateItemText('good', id, text)}
-            deleteItem={id => deleteItem('good', id)}
-          />
-        </section>
-        <section id="bad">
-          <h2>Bad 👎</h2>
-          <List
-            items={bad}
-            addItem={addBad}
-            voteMode={voteMode}
-            upvoteItem={id => upvoteItem(id, 'bad', setBad, bad)}
-            downvoteItem={id => downvoteItem(id, 'bad', setBad, bad)}
-            updateItemText={(id, text) => updateItemText('bad', id, text)}
-            deleteItem={id => deleteItem('bad', id)}
-          />
-        </section>
-        <section id="actions">
-          <h2>Actions ☑️</h2>
-          <List
-            items={actions}
-            addItem={addAction}
-            voteMode={voteMode}
-            upvoteItem={id => upvoteItem(id, 'action', setActions, actions)}
-            downvoteItem={id => downvoteItem(id, 'action', setActions, actions)}
-            updateItemText={(id, text) => updateItemText('actions', id, text)}
-            deleteItem={id => deleteItem('actions', id)}
-          />
-        </section>
-      </article>
+      <Retrospective
+        good={good}
+        setGood={setGood}
+        bad={bad}
+        setBad={setBad}
+        actions={actions}
+        setActions={setActions}
+        setTitle={setTitle}
+        voteMode={voteMode}
+        setVoteMode={setVoteMode}
+        setError={setError}
+        cache={cache}
+      />
     </main>
   );
 }
